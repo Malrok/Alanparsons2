d2a425ea88b736b6b18934d4c341c2c652123b65
Malrok <themalrok@gmail.com>
2013-07-04 12:53:46 +0200

    Ajout de contrôles

--- alanparsons2/src/com/MRK/alanparsons2/Alanparsons2.java
+++ alanparsons2/src/com/MRK/alanparsons2/Alanparsons2.java
@@ -7,34 +7,20 @@ import com.badlogic.gdx.graphics.OrthographicCamera;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.Sprite;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.math.Vector3;
 
 public class Alanparsons2 implements ApplicationListener {
+	private final int SPEED = 100;
+	
 	private OrthographicCamera camera;
 	private SpriteBatch batch;
-//	private Texture texture;
 	private Sprite sprite;
-//	private Rectangle saucisse;
 	
 	@Override
 	public void create() {		
-//		float w = Gdx.graphics.getWidth();
-//		float h = Gdx.graphics.getHeight();
-		
-//		camera = new OrthographicCamera(1, h/w);
 		camera = new OrthographicCamera();
 		camera.setToOrtho(false, 800, 480);
 		batch = new SpriteBatch();
-		
-//		texture = new Texture(Gdx.files.internal("data/saucisse.png"));
-//		texture.setFilter(TextureFilter.Linear, TextureFilter.Linear);
-		
-//		TextureRegion region = new TextureRegion(texture, 0, 0, 512, 275);
-		
-//		saucisse = new Rectangle();
-//		saucisse.x = 800 / 2 - 64 / 2;
-//		saucisse.y = 20;
-//		saucisse.width = 64;
-//		saucisse.height = 64;
 		
 		sprite = new Sprite(new Texture(Gdx.files.internal("data/saucisse.png")));
 		sprite.setSize(64, 64);
@@ -45,7 +31,6 @@ public class Alanparsons2 implements ApplicationListener {
 	@Override
 	public void dispose() {
 		batch.dispose();
-//		texture.dispose();
 	}
 
 	@Override
@@ -56,9 +41,33 @@ public class Alanparsons2 implements ApplicationListener {
 		batch.setProjectionMatrix(camera.combined);
 		batch.begin();
 		sprite.draw(batch);
-//		batch.draw(texture, saucisse.x, saucisse.y);
-
 		batch.end();
+		
+		// process user input
+		if(Gdx.input.isTouched()) {
+			Vector3 touchPos = new Vector3();
+			touchPos.set(Gdx.input.getX(), Gdx.input.getY(), 0);
+			camera.unproject(touchPos);
+			
+//			float x = sprite.getX() + (float) Math.sqrt(Math.abs(touchPos.x - sprite.getX())) / SPEED * Gdx.graphics.getDeltaTime();
+//			float y = sprite.getY() + (float) Math.sqrt(Math.abs(touchPos.y - sprite.getY())) / SPEED * Gdx.graphics.getDeltaTime();
+			float hypo = (float) Math.sqrt((touchPos.x - sprite.getX()) * (touchPos.x - sprite.getX()) + (touchPos.y - sprite.getY()) * (touchPos.y - sprite.getY()));
+			float x = sprite.getX() + ((touchPos.x - sprite.getX()) / hypo) * SPEED * Gdx.graphics.getDeltaTime();
+			float y = sprite.getY() + ((touchPos.y - sprite.getY()) / hypo) * SPEED * Gdx.graphics.getDeltaTime();
+					
+			sprite.setPosition(x, y);
+		}
+//		if(Gdx.input.isKeyPressed(Keys.LEFT)) {
+//			bucket.x -= 200 * Gdx.graphics.getDeltaTime();
+//		}
+//		if(Gdx.input.isKeyPressed(Keys.RIGHT)) {
+//			bucket.x += 200 * Gdx.graphics.getDeltaTime();
+//		}
+		  
+		// make sure the bucket stays within the screen bounds
+//		if(bucket.x < 0) bucket.x = 0;
+//		if(bucket.x > 800 - 64) bucket.x = 800 - 64;
+	      
 	}
 
 	@Override
