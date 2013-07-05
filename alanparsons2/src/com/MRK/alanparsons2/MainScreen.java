0e4bea8bc89ed9f795f2b36fafee411822f5d1d9
Malrok <themalrok@gmail.com>
2013-07-05 16:59:45 +0200

    Ajout d'un bouton dans le MainScreen

--- alanparsons2/src/com/MRK/alanparsons2/MainScreen.java
+++ alanparsons2/src/com/MRK/alanparsons2/MainScreen.java
@@ -1,9 +1,33 @@
 package com.MRK.alanparsons2;
 
+import com.badlogic.gdx.scenes.scene2d.InputEvent;
+import com.badlogic.gdx.scenes.scene2d.InputListener;
+import com.badlogic.gdx.scenes.scene2d.Stage;
+import com.badlogic.gdx.scenes.scene2d.ui.Skin;
+import com.badlogic.gdx.scenes.scene2d.ui.TextButton;
+
 public class MainScreen implements Screen {
 	
+	private Stage stage;
+	
 	public MainScreen() {
+		stage = new Stage(0, 0, true);
 		
+		TextButton button = new TextButton("Start", new Skin());
+		button.addListener(new InputListener() {
+
+//			public boolean touchDown(InputEvent event, float x, float y, int pointer, int button) {
+//		 		Gdx.app.log("Example", "touch started at (" + x + ", " + y + ")");
+//		 		return false;
+//		 	}
+		 
+		 	public void touchUp (InputEvent event, float x, float y, int pointer, int button) {
+//		 		Gdx.app.log("Example", "touch done at (" + x + ", " + y + ")");
+		 		
+		 	}
+			
+		});
+		stage.addActor(button);
 	}
 	
 	@Override
